9136d28693eebceccc2cef6a7bc17ec1d4a49d2e
Scott <stsmith@callutheran.edu>
2019-09-11 15:50:40 -0700

    added bootstrap markup

--- header.php
+++ header.php
@@ -17,41 +17,46 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<link rel="stylesheet" href="https://www.callutheran.edu/_resources/css/styles.css">
+
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'regals_way' ); ?></a>
+<body <?php body_class('bg-gray'); ?>>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'regals_way' ); ?></a>
+
+	<header id="masthead" class="container site-header">
+		<div class="row site-branding">
+			<div class="col-sm-12">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$regals_way_description = get_bloginfo( 'description', 'display' );
-			if ( $regals_way_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $regals_way_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$regals_way_description = get_bloginfo( 'description', 'display' );
+				if ( $regals_way_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $regals_way_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'regals_way' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
+		<nav id="site-navigation" class="row main-navigation">
+			<div class="col-sm-12">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'regals_way' ); ?></button>
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+			</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
