64aba61e6e2f88205c7b2567845f43a2b62616e3
Scott <stsmith@callutheran.edu>
2019-09-11 15:51:05 -0700

    cleaned up

--- template-parts/content.php
+++ template-parts/content.php
@@ -21,15 +21,11 @@
 		if ( 'post' === get_post_type() ) :
 			?>
 			<div class="entry-meta">
-				<?php
-				regals_way_posted_on();
-				regals_way_posted_by();
-				?>
+
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php regals_way_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
@@ -54,6 +50,6 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php regals_way_entry_footer(); ?>
+
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
