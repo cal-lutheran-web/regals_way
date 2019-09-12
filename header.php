b77601d67084ddc3025435c1bda95412aacf0ee8
Scott <stsmith@callutheran.edu>
2019-09-12 14:48:29 -0700

    early styles

--- header.php
+++ header.php
@@ -17,47 +17,37 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<link rel="stylesheet" href="https://www.callutheran.edu/_resources/css/styles.css">
+	<!-- <link rel="stylesheet" href="https://www.callutheran.edu/_resources/css/styles.css"> -->
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
 
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class('bg-gray'); ?>>
+<body <?php body_class(); ?>>
 
 	<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'regals_way' ); ?></a>
 
 	<header id="masthead" class="container site-header">
-		<div class="row site-branding">
-			<div class="col-sm-12">
-				<?php
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-				$regals_way_description = get_bloginfo( 'description', 'display' );
-				if ( $regals_way_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $regals_way_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-			</div>
-		</div><!-- .site-branding -->
+		<div class="site-branding">
+			<?php if(is_front_page() && is_home()){ ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="site-title-mark"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php } else { ?>
+				<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="site-title-mark"><?php bloginfo( 'name' ); ?></a></div>
+			<?php } ?>
+
+			<p class="site-description"><?php bloginfo('description'); ?></p>
+			
+		</div>
 
 		<nav id="site-navigation" class="row main-navigation">
-			<div class="col-sm-12">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'regals_way' ); ?></button>
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-				?>
-			</div>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'regals_way' ); ?></button>
+			<?php
+			// wp_nav_menu( array(
+			// 	'theme_location' => 'menu-1',
+			// 	'menu_id'        => 'primary-menu',
+			// ) );
+			?>
+		</nav>
+	</header>
 
-	<div id="content" class="site-content">
+	<div id="content" class="container site-content">
