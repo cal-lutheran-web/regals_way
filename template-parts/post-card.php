b63a3fa992c8108b1df464d83f00bd171c4d4f0e
Scott Smith <scottsmith24@gmail.com>
2019-09-23 17:09:21 -0700

    added featured images to post card

--- template-parts/post-card.php
+++ template-parts/post-card.php
@@ -7,10 +7,18 @@
 		$post_tags = 'theme-'.$item->slug.' ';
 	}
 
+	if(has_post_thumbnail( $post->ID )){
+		$featured_img = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID ), 'medium-large' )[0];
+	} else {
+		$featured_img = '';
+	}	
+	
 ?>
 
 <article class="post-card <?php echo $post_tags; ?>">
 	<header><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></header>
-	<figure class="post-card-image"></figure>
+	<figure class="post-card-image" style="background-image: url('<?php echo $featured_img; ?>');"></figure>
+	<?php if($hide_theme_icon !== true){ ?>
 	<figure class="theme-icon size-small"></figure>
+	<?php } ?>
 </article>
