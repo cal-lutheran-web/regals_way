1e5486e8b0c2e29a646af190d68e159feb847980
Scott Smith <scottsmith24@gmail.com>
2019-10-17 11:17:07 -0700

    fixed float clearing

--- functions.php
+++ functions.php
@@ -8,6 +8,15 @@ include 'functions/custom-fields.php';
 // add editor styles
 add_editor_style('editor-style.css');
 
+
+// remove p tag around images
+function filter_ptags_on_images($content){
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+ 
+add_filter('the_content', 'filter_ptags_on_images');
+
+
 // remove native categories and tags from admin menu, we will do our own
 function remove_menus(){
 	remove_menu_page('edit-comments.php');
